37ea21964e4d11bcb9b6cc6d9c0bccf7ce5f2caa
Honey Syal <honey.syal@talink.io>
2025-11-13 16:44:21 +0530

    flutter app setup

---
+++ app/lib/constants/colors.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6C63FF);
+  static const Color secondaryColor = Color(0xFF2D3142);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color textPrimary = Color(0xFF2D3142);
+  static const Color textSecondary = Color(0xFF8D8D8D);
+  static const Color errorColor = Color(0xFFE63946);
+  static const Color successColor = Color(0xFF06D6A0);
+  static const Color white = Color(0xFFFFFFFF);
+}
